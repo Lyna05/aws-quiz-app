3059dd080a4460ee339cfe106b863b4a52f45666
Emanuel crisan <emanuelcrisan778@gmail.com>
2024-06-05 18:51:52 +0200

    proben

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,96 +1,80 @@
-// eslint-disable-next-line no-unused-vars
-import * as React from 'react'
-import'./Quiz.css'
+import { useState } from 'react';
 import { data } from '../../assets/data';
-import {useState} from 'react'
-import {useRef} from 'react'
+import './Quiz.css';
 
 const Quiz = () => {
-    let [index,setIndex] = useState(0);
-    let [question,setQuestion] = useState(data[index]);
-    let [lock,setLock] = useState(false);
-    let [score,setScore] = useState(0);
-    let [result,setResult] = useState (false);
+    const [index, setIndex] = useState(0);
+    const [lock, setLock] = useState(false);
+    const [score, setScore] = useState(0);
+    const [result, setResult] = useState(false);
+    const [feedback, setFeedback] = useState('');
+    const [info, setInfo] = useState('');
 
-    let Option1 = useRef(null);
-    let Option2 = useRef(null);
-    let Option3 = useRef(null);
-    let Option4 = useRef(null);
+    const question = data[index]; // Aktuelle Frage basierend auf dem Index
 
-    let option_array = [Option1,Option2,Option3,Option4];
-
-    const checkAns = (e,ans) => {
-        if (lock === false) {
-            if (question.ans===ans) {
+    const checkAns = (e, ans) => {
+        if (!lock) {
+            if (question.ans === ans) {
                 e.target.classList.add("correct");
                 setLock(true);
-                setScore(prev=>prev+1);
-
+                setScore(prev => prev + 1);
+                setFeedback('Richtig!');
+            } else {
+                e.target.classList.add("wrong");
+                setLock(true);
+                setFeedback('Leider falsch.');
+            }
+            setInfo(question.info); // Setze die Info zur richtigen Antwort
         }
-        else{
-            e.target.classList.add("wrong");
-            setLock(true);
-            option_array[question.ans-1].current.classList.add("correct");
-        }
-    } 
     }
-    const next = ()=>{
-        if (lock===true) {
-            if (index === data.length -1) {
+
+    const next = () => {
+        if (lock) {
+            if (index === data.length - 1) {
                 setResult(true);
                 return 0;
-
             }
-            setIndex(++index);
-            setQuestion(data[index]);
+            setIndex(prev => prev + 1); // Erhöhe den Index für die nächste Frage
             setLock(false);
-            option_array.map((option)=>{
-                option.current.classList.remove("wrong");
-                option.current.classList.remove("correct");
-                return null;
-
-
-            })
+            setFeedback('');
+            setInfo('');
         }
-
     }
 
     const reset = () => {
         setIndex(0);
-        setQuestion(data[0]);
         setScore(0);
-        setLock(false);
         setResult(false);
-
-
+        setFeedback('');
+        setInfo('');
     }
 
     return (
         <div className='container'>
             <h1>Aws Quiz-App</h1>
             <hr />
-            {result?<></>:<>  
-            <h2>{index+1}. {question.question}</h2>
-            <ul>
-                <li ref={Option1}onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
-                <li ref={Option2}onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
-                <li ref={Option3}onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
-                <li ref={Option4}onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
-            </ul>
-            <button onClick={next}>Next</button>
-            <div className="index">{index+1} of {data.length} questions</div></>}
-            {result?<>  
-            <h2>You Scored {score} out of {data.length}</h2>
-            <button onClick={reset}>Reset</button>
-            </>:<></>}
-          
-            
-            
-          
-
-            
+            {!result ?
+                <>
+                    <h2>{index + 1}. {question.question}</h2>
+                    <ul>
+                        <li onClick={(e) => { checkAns(e, 1) }}>{question.option1}</li>
+                        <li onClick={(e) => { checkAns(e, 2) }}>{question.option2}</li>
+                        <li onClick={(e) => { checkAns(e, 3) }}>{question.option3}</li>
+                        <li onClick={(e) => { checkAns(e, 4) }}>{question.option4}</li>
+                    </ul>
+                    <button onClick={next}>Weiter</button>
+                    <div className="index">{index + 1} von {data.length} Fragen</div>
+                    {feedback && <div>{feedback}</div>}
+                    {info && <div>Zusätzliche Informationen: {info}</div>}
+                </>
+                :
+                <>
+                    <h2>Du hast {score} von {data.length} Punkten erreicht</h2>
+                    <button onClick={reset}>Neustart</button>
+                </>
+            }
         </div>
     )
 }
 
-export default Quiz
+export default Quiz;
