8259ffea8d186cd0a88753b5a411e504351d7d01
Gülcan Kamat <gulcankamat@gmail.com>
2024-06-05 03:47:46 +0200

    Adding the data.js file

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+      question: "Welcher AWS-Service bietet einen vollständig verwalteten NoSQL-Datenbankdienst?",
+      option1: "Amazon S3",
+      option2: "Amazon RDS",
+      option3: "Amazon DynamoDB",
+      option4: "Amazon Redshift",
+      ans: 3,
+    },
+    {
+      question: "Welcher AWS-Service bietet eine skalierbare und verwaltete Container-Orchestrierung?",
+      option1: "Amazon EC2",
+      option2: "Amazon EKS",
+      option3: "Amazon ECS",
+      option4: "Amazon Lambda",
+      ans: 2,
+    },
+    {
+      question: "Welcher AWS-Service bietet einen vollständig verwalteten Stream-Verarbeitungsdienst?",
+      option1: "Amazon SQS",
+      option2: "Amazon SNS",
+      option3: "Amazon Kinesis",
+      option4: "Amazon RDS",
+      ans: 3,
+    },
+    {
+      question: "Welcher AWS-Service bietet eine Möglichkeit zur Verwaltung von Identitäten und Zugriff auf AWS-Ressourcen?",
+      option1: "Amazon Route 53",
+      option2: "Amazon IAM",
+      option3: "Amazon CloudFront",
+      option4: "Amazon Cognito",
+      ans: 2,
+    },
+    {
+      question: "Welcher AWS-Service bietet einen vollständig verwalteten Dateispeicherdienst?",
+      option1: "Amazon EBS",
+      option2: "Amazon Glacier",
+      option3: "Amazon S3",
+      option4: "Amazon RDS",
+      ans: 3,
+    },
+  ];
