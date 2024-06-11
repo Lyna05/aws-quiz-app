3d98682c52f7c055349847c593f0c58e7ff87274
Gülcan Kamat <gulcankamat@gmail.com>
2024-06-11 19:53:15 +0200

    Update the last version of aws-quiz.

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { data } from '../../assets/data';
+import { correctInfo } from '../../assets/correctInfo';
 import './Quiz.css';
 
 function Quiz() {
@@ -9,26 +10,50 @@ function Quiz() {
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [rating, setRating] = useState(0);
   const [showResult, setShowResult] = useState(false);
-  const [showTempResult, setShowTempResult] = useState(false);
   const [shuffledQuestions, setShuffledQuestions] = useState([]);
   const [isRandomMode, setIsRandomMode] = useState(false);
   const [feedback, setFeedback] = useState("");
   const [showFeedbackForm, setShowFeedbackForm] = useState(false);
+  const [answeredQuestions, setAnsweredQuestions] = useState([]);
+  const [markedQuestions, setMarkedQuestions] = useState([]);
+  const [timeLeft, setTimeLeft] = useState(40 * 60); // 40 minutes in seconds
+  const [timerActive, setTimerActive] = useState(true);
+  const [showReviewModal, setShowReviewModal] = useState(false); // State for review modal
+  const [showMarkedQuestionsModal, setShowMarkedQuestionsModal] = useState(false); // State for marked questions modal
+
+  const timerRef = useRef(null);
 
   useEffect(() => {
-    if (showResult) {
-      setShowTempResult(true);
-      const timer = setTimeout(() => {
-        setShowTempResult(false);
-        setShowResult(false);
-      }, 5000);
-      return () => clearTimeout(timer);
+    if (timerActive) {
+      timerRef.current = setInterval(() => {
+        setTimeLeft((prevTimeLeft) => {
+          if (prevTimeLeft <= 1) {
+            clearInterval(timerRef.current);
+            playSound();
+            alert('Ihre Zeit ist vorüber!');
+            setShowResult(true);
+            return 0;
+          }
+          return prevTimeLeft - 1;
+        });
+      }, 1000);
+    } else {
+      clearInterval(timerRef.current);
     }
-  }, [showResult]);
+
+    return () => clearInterval(timerRef.current);
+  }, [timerActive]);
+
+  const playSound = () => {
+    const audio = new Audio('/path/to/sound.mp3'); // Add the path to your sound file
+    audio.play();
+  };
 
   const handleOptionClick = (optionIndex) => {
     setSelectedOption(optionIndex);
     setShowAnswer(true);
+    const isCorrect = optionIndex === data[currentQuestion].ans;
+    setAnsweredQuestions([...answeredQuestions, { question: currentQuestion, isCorrect }]);
   };
 
   const handleNextQuestion = () => {
@@ -42,6 +67,10 @@ function Quiz() {
       setCurrentQuestion((prevQuestion) => (prevQuestion + 1) % shuffledQuestions.length);
     } else {
       setCurrentQuestion((prevQuestion) => (prevQuestion + 1) % data.length);
+    }
+
+    if (currentQuestion + 1 === totalQuestions) {
+      setShowResult(true);
     }
   };
 
@@ -65,15 +94,16 @@ function Quiz() {
     setShowFeedbackForm(!showFeedbackForm);
   };
 
-  const handleShowResult = () => {
-    setShowResult(true);
-  };
-
   const handleRandomQuestions = () => {
-    const shuffled = data.map((_, index) => index).sort(() => Math.random() - 0.5);
-    setShuffledQuestions(shuffled);
-    setIsRandomMode(true);
-    setCurrentQuestion(shuffled[0]);
+    if (isRandomMode) {
+      setIsRandomMode(false);
+      setShuffledQuestions([]);
+    } else {
+      const shuffled = data.map((_, index) => index).sort(() => Math.random() - 0.5);
+      setShuffledQuestions(shuffled);
+      setIsRandomMode(true);
+      setCurrentQuestion(shuffled[0]);
+    }
   };
 
   const handleQuestionClick = (index) => {
@@ -104,12 +134,72 @@ function Quiz() {
     console.log("Sending email with content:", content);
   };
 
+  const handleRestart = () => {
+    setCurrentQuestion(0);
+    setSelectedOption(null);
+    setShowAnswer(false);
+    setCorrectAnswers(0);
+    setRating(0);
+    setShowResult(false);
+    setShuffledQuestions([]);
+    setIsRandomMode(false);
+    setFeedback("");
+    setShowFeedbackForm(false);
+    setAnsweredQuestions([]);
+    setMarkedQuestions([]);
+    setTimeLeft(40 * 60); // Reset timer
+    setTimerActive(true); // Start timer on restart
+  };
+
+  const handleMarkQuestion = () => {
+    if (!markedQuestions.includes(currentQuestion)) {
+      setMarkedQuestions([...markedQuestions, currentQuestion]);
+    } else {
+      setMarkedQuestions(markedQuestions.filter(q => q !== currentQuestion));
+    }
+  };
+
+  const handleReviewAttempt = () => {
+    setShowReviewModal(true);
+  };
+
+  const closeReviewModal = () => {
+    setShowReviewModal(false);
+  };
+
+  const handleShowMarkedQuestions = () => {
+    setShowMarkedQuestionsModal(true);
+  };
+
+  const closeMarkedQuestionsModal = () => {
+    setShowMarkedQuestionsModal(false);
+  };
+
   const question = isRandomMode ? data[shuffledQuestions[currentQuestion]] : data[currentQuestion];
   const totalQuestions = data.length;
   const accuracy = (correctAnswers / totalQuestions) * 100;
 
+  const getQuestionClass = (index) => {
+    const answeredQuestion = answeredQuestions.find(q => q.question === index);
+    if (!answeredQuestion) return '';
+    return answeredQuestion.isCorrect ? 'correct' : 'wrong';
+  };
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  };
+
   return (
     <div className="quiz-container">
+      <div className="header-buttons">
+        <button className="restart-button" onClick={handleRestart}>Neustart</button>
+        
+      </div>
+      <div className="timer">
+        {formatTime(timeLeft)}
+      </div>
       <div className="quiz-content">
         <p className="question-number">Frage {currentQuestion + 1} von {totalQuestions}</p>
         <p className="question-text">{question.question}</p>
@@ -147,19 +237,45 @@ function Quiz() {
 
         <div className="answer-feedback-container">
           <button onClick={handleShowAnswer} className="quiz-button">Antwort-Anzeigen</button>
-          <button onClick={handleFeedbackToggle} className="quiz-button">Feedback-Senden</button>
-          <button onClick={handleRandomQuestions} className="quiz-button">Random-Fragen</button>
+          <button onClick={handleMarkQuestion} className="quiz-button">
+            {markedQuestions.includes(currentQuestion) ? 'Frage entmarkieren' : 'Frage markieren'}
+          </button>
+          <button onClick={handleShowMarkedQuestions} className="quiz-button">Markierten Fragen anzeigen</button>
+          <button onClick={handleRandomQuestions} className="quiz-button">
+            {isRandomMode ? 'Random-Fragen deaktivieren' : 'Random-Fragen aktivieren'}
+          </button>
         </div>
 
         {showAnswer && (
           <div className="full-answer">
-            <p className="correct-answer">{question.info}</p>
+            <p className="correct-answer">{correctInfo[currentQuestion]}</p>
             <p className="wrong-answer">{question.wrongInfo.option1}</p>
             <p className="wrong-answer">{question.wrongInfo.option2}</p>
             <p className="wrong-answer">{question.wrongInfo.option3}</p>
             <p className="wrong-answer">{question.wrongInfo.option4}</p>
           </div>
         )}
+
+        {showResult && (
+          <div className="result-container">
+            <p>Sie haben {correctAnswers} von {totalQuestions} Fragen richtig beantwortet ({accuracy.toFixed(2)}% Richtigkeit).</p>
+          </div>
+        )}
+
+        <div className="video-container">
+          <p className="video-title">Erklärungsvideo</p>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/3XFODda6YXo"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+
+        <button onClick={handleFeedbackToggle} className="quiz-button">Feedback Senden</button>
 
         {showFeedbackForm && (
           <div className="feedback-form">
@@ -186,41 +302,78 @@ function Quiz() {
           </div>
         )}
 
-        {currentQuestion === data.length - 1 && (
-          <button onClick={handleShowResult} className="show-result-button">
-            Ergebnis-Anzeigen
-          </button>
-        )}
+      </div>
 
-        <div className="video-container">
-          <p className="video-title">Erklärungsvideo</p>
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/3XFODda6YXo"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+      <div className="side-panel">
+        <button className="review-attempt-button side" onClick={handleReviewAttempt}>Überprüfung der Antworten</button>
+        <div className="dot-container">
+          {[...Array(totalQuestions)].map((_, index) => (
+            <div
+              key={index}
+              className={`dot ${getQuestionClass(index)} ${index === currentQuestion ? 'current' : ''} ${markedQuestions.includes(index) ? 'marked' : ''}`}
+              onClick={() => handleQuestionClick(index)}
+            >
+              {index + 1}
+            </div>
+          ))}
         </div>
       </div>
 
-      <div className="dot-container">
-        {[...Array(totalQuestions)].map((_, index) => (
-          <div
-            key={index}
-            className={`dot ${index === currentQuestion ? 'current' : ''}`}
-            onClick={() => handleQuestionClick(index)}
-          >
-            {index + 1}
+      {showReviewModal && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <button className="close-modal" onClick={closeReviewModal}>X</button>
+            <h3>Deine Antworten</h3>
+            <table className="review-table">
+              <thead>
+                <tr>
+                  <th>Nummer</th>
+                  <th>Frage</th>
+                  <th>Ergebnis</th>
+                  <th>Öffnen</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((q, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{q.question}</td>
+                    <td>{answeredQuestions.find(a => a.question === index) ? answeredQuestions.find(a => a.question === index).isCorrect ? 'Correct' : 'Wrong' : 'Unanswered'}</td>
+                    <td><button onClick={() => handleQuestionClick(index)}>Öffnen</button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
 
-      {showTempResult && (
-        <div className="result-container">
-          <p>Sie haben {correctAnswers} von {totalQuestions} Fragen richtig beantwortet ({accuracy.toFixed(2)}% Richtigkeit).</p>
+      {showMarkedQuestionsModal && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <button className="close-modal" onClick={closeMarkedQuestionsModal}>X</button>
+            <h3>Markierten Fragen</h3>
+            <table className="review-table">
+              <thead>
+                <tr>
+                  <th>Nummer</th>
+                  <th>Frage</th>
+                  <th>Ergebnis</th>
+                  <th>Öffnen</th>
+                </tr>
+              </thead>
+              <tbody>
+                {markedQuestions.map((index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{data[index].question}</td>
+                    <td>{answeredQuestions.find(a => a.question === index) ? answeredQuestions.find(a => a.question === index).isCorrect ? 'Correct' : 'Wrong' : 'Unanswered'}</td>
+                    <td><button onClick={() => handleQuestionClick(index)}>Öffnen</button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
 
