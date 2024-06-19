4ce227724f0ce7a03f6ef11e13995cf9c84b36e1
Gülcan Kamat <gulcankamat@gmail.com>
2024-06-19 09:42:40 +0200

    Updating the routes of login page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
 import Login from './components/Login';
-import Dashboard from './Dashboard';
-import PrivateRoute from './PrivateRoute';
 import ChangePassword from './components/ChangePassword';
 import Quiz from './components/Quiz/Quiz';
-import SignUp from './components/SignUp'; // Importiere die SignUp-Komponente
+import SignUp from './components/SignUp'; 
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<PrivateRoute component={Dashboard} />} />
       <Route path="/change-password" element={<ChangePassword />} />
       <Route path="/quiz" element={<Quiz />} /> 
       <Route path="/sign-up" element={<SignUp />} /> 
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login />} />
     </Routes>
   </Router>
 );
