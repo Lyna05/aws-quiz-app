8413bd3ec5df66d8f3e30c4c7c35d254f6daf0b7
Gülcan Kamat <gulcankamat@gmail.com>
2024-06-25 12:32:55 +0200

    Added the domain to the right answers.

--- src/assets/correctInfo.js
+++ src/assets/correctInfo.js
@@ -1,28 +1,21 @@
-
-
 export const correctInfo = [
-    "Amazon DynamoDB ist ein vollständig verwalteter NoSQL-Datenbankdienst, der schnelle Leistung und globale Skalierbarkeit bietet.",
-    "Amazon Elastic Kubernetes Service (Amazon EKS) ist ein vollständig verwalteter Containerorchestrierungsdienst, der auf Kubernetes basiert.",
-    "Amazon S3 (Simple Storage Service) ist ein skalierbarer und zuverlässiger Speicherdienst, der zum Speichern und Abrufen von Daten jederzeit verwendet werden kann.",
-    "Amazon Glacier ist ein kostengünstiger Speicherdienst, der sich ideal für die Archivierung und Sicherung von Daten eignet, die nicht häufig benötigt werden.",
-    "Amazon Auto Scaling ist ein Dienst, der die Rechenkapazität automatisch an die Anforderungen der Anwendung anpasst.",
-    "AWS KMS (Key Management Service) ist ein Dienst zur Erstellung und Verwaltung von Verschlüsselungsschlüsseln, die zur Verschlüsselung und Entschlüsselung von Daten in AWS verwendet werden.",
-    "Amazon VPC (Virtual Private Cloud) ermöglicht es Benutzern, ein isoliertes virtuelles Netzwerk in der AWS-Cloud zu erstellen.",
-    "Amazon Aurora ist ein vollständig verwalteter relationale Datenbankdienst, der für Leistung, Verfügbarkeit und Skalierbarkeit optimiert ist.",
-    "AWS IAM (Identity and Access Management) ist ein Dienst zur Verwaltung von Identitäten und dem Zugriff auf AWS-Ressourcen.",
-    "Amazon SNS (Simple Notification Service) ist ein Dienst zum Senden von Nachrichten an verschiedene Endpunkte wie E-Mail, HTTP und AWS Lambda.",
-    "Amazon RDS (Relational Database Service) ist ein vollständig verwalteter Dienst zum Erstellen, Betreiben und Skalieren relationaler Datenbanken in der Cloud.",
-    "Amazon S3 (Simple Storage Service) ist ein Objektspeicherdienst, der Skalierbarkeit, Haltbarkeit und hohe Leistung bietet.",
-    "AWS Lambda ist ein Dienst, der Code als Reaktion auf Ereignisse ausführt, ohne die Serverinfrastruktur zu verwalten.",
-    "AWS CloudFormation ist ein Dienst zur Verwaltung von AWS-Ressourcen mittels Code, um die Infrastruktur als Code zu definieren.",
-    "Amazon RDS (Relational Database Service) ist ein vollständig verwalteter relationale Datenbankdienst.",
-    "Amazon EC2 (Elastic Compute Cloud) ist ein Dienst, der skalierbare Rechenkapazität in der Cloud bereitstellt.",
-    "AWS Lambda ist ein Dienst, der Code als Reaktion auf Ereignisse ausführt, ohne die Serverinfrastruktur zu verwalten.",
-    "AWS Device Farm ist ein Dienst, der Tools für das automatisierte Testen von mobilen Anwendungen bereitstellt.",
-    "AWS Backup ist ein Dienst, der eine automatisierte Backup-Lösung für Amazon EC2-Instanzen bietet.",
-    "Amazon EFS (Elastic File System) ist ein Dienst, der ein verteiltes, skalierbares und serverloses Dateisystem bietet.",
-    "Amazon API Gateway ist ein Dienst zur Erstellung, Veröffentlichung, Wartung und Absicherung von APIs.",
-    "Amazon S3 (Simple Storage Service) ist ein Objektspeicherdienst, der Haltbarkeit, Skalierbarkeit und hohe Leistung bietet.",
-    "Amazon CloudWatch ist ein Dienst zur Überwachung und Verwaltung von Ressourcen und Anwendungen in der Cloud."
-  ];
-  
+  "Domain:1 - Amazon DynamoDB ist ein vollständig verwalteter NoSQL-Datenbankdienst, der schnelle Leistung und globale Skalierbarkeit bietet.",
+  "Domain:2 - Amazon Elastic Kubernetes Service (Amazon EKS) ist ein vollständig verwalteter Containerorchestrierungsdienst, der auf Kubernetes basiert.",
+  "Domain:3 - Amazon S3 (Simple Storage Service) ist ein skalierbarer und zuverlässiger Speicherdienst, der zum Speichern und Abrufen von Daten jederzeit verwendet werden kann.",
+  "Domain:4 - Amazon Glacier ist ein kostengünstiger Speicherdienst, der sich ideal für die Archivierung und Sicherung von Daten eignet, die nicht häufig benötigt werden.",
+  "Domain:5 - Amazon Auto Scaling ist ein Dienst, der die Rechenkapazität automatisch an die Anforderungen der Anwendung anpasst.",
+  "Domain:6 - AWS KMS (Key Management Service) ist ein Dienst zur Erstellung und Verwaltung von Verschlüsselungsschlüsseln, die zur Verschlüsselung und Entschlüsselung von Daten in AWS verwendet werden.",
+  "Domain:1 - Amazon ECS (Elastic Container Service) ist ein vollständig verwalteter Containerorchestrierungsdienst von AWS, der das Ausführen und Skalieren von Container-Workloads in der Cloud ermöglicht.",
+  "Domain:2 - Amazon RDS (Relational Database Service) ist ein vollständig verwalteter Dienst zum Erstellen, Betreiben und Skalieren relationaler Datenbanken in der Cloud.",
+  "Domain:3 - Amazon EC2 (Elastic Compute Cloud) ist ein Dienst, der skalierbare Rechenkapazität in der Cloud bereitstellt.",
+  "Domain:4 - Amazon Athena ist ein interaktiver Abfrage-Service, der es ermöglicht, Daten in Amazon S3 zu analysieren und Abfragen in Echtzeit auszuführen.",
+  "Domain:5 - Amazon SageMaker ist ein vollständig verwalteter Dienst, der es Entwicklern und Datenwissenschaftlern ermöglicht, Machine Learning-Modelle einfach zu erstellen, zu trainieren und bereitzustellen.",
+  "Domain:6 - AWS Lambda ist ein serverloser Berechnungsdienst, der es Entwicklern ermöglicht, Code auszuführen, ohne sich um die Bereitstellung oder Verwaltung von Servern kümmern zu müssen.",
+  "Domain:1 - Amazon SQS (Simple Queue Service) ist ein verwalteter Warteschlangenservice, der es ermöglicht, Nachrichten zwischen verteilten Systemen und Microservices sicher und zuverlässig zu übertragen.",
+  "Domain:2 - AWS CloudFormation ist ein Dienst zur Verwaltung von AWS-Ressourcen mittels Code, um die Infrastruktur als Code zu definieren.",
+  "Domain:3 - Amazon Kinesis ist ein verwalteter Streaming-Dienst, der es ermöglicht, Daten in Echtzeit zu sammeln, zu verarbeiten und zu analysieren.",
+  "Domain:4 - Amazon CloudWatch ist ein Überwachungsdienst, der es Kunden ermöglicht, Ressourcen und Anwendungen in der Cloud zu überwachen und Protokolle, Metriken und Alarme zu zentralisieren.",
+  "Domain:5 - Amazon ELB (Elastic Load Balancing) ist ein Dienst, der den Datenverkehr gleichmäßig auf mehrere EC2-Instanzen verteilt, um die Auslastung zu verteilen und die Verfügbarkeit der Anwendungen zu verbessern.",
+  "Domain:3 - AWS Direct Connect bietet eine dedizierte Netzwerkverbindung zwischen Ihrem Rechenzentrum und AWS, um eine private und direkte Verbindung zu AWS-Ressourcen herzustellen.",
+  "Domain:4 - Amazon GuardDuty ist ein verwalteter Sicherheitsdienst, der die Sicherheit von AWS-Konten, -Daten und -Workloads überwacht und kontinuierlich Verbesserungen vorschlägt."
+];
